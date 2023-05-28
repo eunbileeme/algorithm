29fdc451e1e96e1cc47e3b4091151d704bcb5b60
EunBi Lee <pask2202@gmail.com>
2023-05-29 00:35:29 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,7 +1,10 @@
 class Solution {
     public String solution(int num) {
-        if (num % 2 == 0)
+        if (num % 2 == 0) {
             return "Even";
-        else return "Odd";
+        }
+        else {
+            return "Odd";
+        }
     }
 }
