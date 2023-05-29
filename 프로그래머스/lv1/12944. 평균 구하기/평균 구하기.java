f41f3cca5523419a667b95076476676a8c7cef69
EunBi Lee <pask2202@gmail.com>
2023-05-29 17:34:28 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        
+        for (int i = 0; i < arr.length; i ++) {
+            sum = sum + arr[i];
+        }
+        
+        answer = sum / arr.length;
+        return answer;
+    }
+}
