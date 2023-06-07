3ceeafae69adc1923d294d6c18dd342da7eb6708
Eunbi Lee <pask2202@gmail.com>
2023-06-07 17:35:25 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.07 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,14 +1,12 @@
 class Solution {
-  public long solution(long n) {
-      long answer = 0;
-
-      for (long i = 1; i <= n; i++) {
-          if (i * i == n) {
-              answer = (i + 1) * (i + 1);
-              break;
-          }
-          else answer = -1;
-      }
-      return answer;
-  }
+    public long solution(long n) {
+        // 일반적으로 아래와 같은 형 변환은 데이터 손실의 우려가 있음
+        // 하지만, 이 경우 제곱근의 정수 부분만 필요하므로 상관 x
+        long x = (long) Math.sqrt(n);
+        
+        if (x * x == n) {
+            return (x + 1) * (x + 1);
+        }
+        return -1;
+    }
 }
