c5097f4f03fc56c8d9fd19c4be5d603bf5ca618b
Eunbi Lee <pask2202@gmail.com>
2023-06-14 21:32:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 10.84 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
-        int index = 0;
+        // indexOf() : ArrayList 자료구조에서만 지원
+        int index = Arrays.asList(seoul).indexOf("Kim");
         
         for (String str : seoul) {
             if (str.equals("Kim")) {
-                // indexOf() : ArrayList 자료구조에서만 지원
-                index += Arrays.asList(seoul).indexOf("Kim");
                 answer += "김서방은 " + index + "에 있다";
             }
         }
