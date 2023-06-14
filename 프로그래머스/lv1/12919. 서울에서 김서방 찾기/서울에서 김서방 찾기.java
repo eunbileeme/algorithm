1d5ff25dee5f49d4d045dca409c7e7c902277508
Eunbi Lee <pask2202@gmail.com>
2023-06-14 21:38:03 +0900

    Update 서울에서 김서방 찾기.java

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -4,7 +4,6 @@ class Solution {
     public String solution(String[] seoul) {
         String answer = "";
         int index = Arrays.asList(seoul).indexOf("Kim"); // indexOf() : ArrayList 자료구조에서만 지원
-        
         answer += "김서방은 " + index + "에 있다";
         
         return answer;
