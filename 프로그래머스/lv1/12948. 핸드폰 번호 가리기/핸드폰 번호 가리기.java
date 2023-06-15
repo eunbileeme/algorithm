49ff0bd11fecdd5bd99787470e39cd7fda5a0fb3
Eunbi Lee <pask2202@gmail.com>
2023-06-15 17:16:43 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,13 +1,17 @@
+// String -> char array -> String
+
 import java.util.*;
 
 class Solution {
     public String solution(String phone_number) {
+        // .toCharArray() : String -> char array
         char[] arr = phone_number.toCharArray();
         int answer = phone_number.length() - 4;
         
         for (int i = 0; i < answer; i ++) {
             arr[i] = '*';
         }
+        // String.valueOf() : element를 합쳐서 String으로 반환
         return String.valueOf(arr);
     }
 }
