bd1876204f2f2ae5edef8b42cbe3ea99a71da57d
Eunbi Lee <pask2202@gmail.com>
2023-06-15 15:26:18 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for (int i = 0; i < absolutes.length; i ++) {
+            if (signs[i] == true) {
+                absolutes[i] = absolutes[i];
+            }
+            else {
+                absolutes[i] = - absolutes[i];
+            }
+        }
+        
+        for (int i = 0; i < absolutes.length; i ++) {
+            answer += absolutes[i];
+        }
+        
+        return answer;
+    }
+}
