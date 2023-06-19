7a5fe538c3ea83d57ec4a95f7636e1faa0cf3bde
Eunbi Lee <pask2202@gmail.com>
2023-06-20 01:04:28 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.82 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,4 +1,4 @@
-// ※ Object[]는 정적인 배열이므로 값을 삭제할 수 없음
+// Object[]는 정적인 배열이므로 값을 삭제할 수 없음
 // 따라서 array -> arrayList -> 삭제 작업 수행
 
 import java.util.*;
@@ -6,8 +6,10 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         int[] array = arr;
+        // Integer -> int: o,
+        // int -> Integer: x
         List<Integer> list = new ArrayList<>();
-        
+        // int[] -> ArrayList
         for (Integer num : array) {
             list.add(num);
         }
@@ -17,9 +19,11 @@ class Solution {
             list.add(-1);
         }
         else {
+            // Collections.min(): ArrayList 최솟값 구하기
             list.remove(Collections.min(list));
         }
         
+        // ArrayList -> int[]
         int[] answer = list.stream().mapToInt(i->i).toArray();
         
         return answer;
