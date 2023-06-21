a0f8674158b7a0d0cbfd729d43975e72e045b6c7
Eunbi Lee <pask2202@gmail.com>
2023-06-21 20:52:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                answer.append("수");
+            } else {
+                answer.append("박");
+            }
+        }
+        
+        return answer.toString();
+    }
+}
