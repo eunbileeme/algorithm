d91333c068cb77d36d73176948ba927d953fe0f9
Eunbi Lee <pask2202@gmail.com>
2023-06-28 20:33:24 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 3.69 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,32 +1,21 @@
-// 문자열을 내림차순에 따라 정렬하면
-// 대문자 + 소문자 순으로 정렬됨
-// ASCII 기준, 대문자가 소문자보다 값이 작기 때문
-
-// 그 외 풀이 : https://devmoony.tistory.com/61
-
 import java.util.Arrays;
 import java.util.Collections;
 
 class Solution {
     public String solution(String s) {
-        // s.chars() = IntStream(primitive Stream) = int
-        // 따라서 Character 객체로 만들기 위해 (char) 강제 형변환 수행
+        // 문자(char) 배열로 변환했어도 동작했을까?
+        // 1) .chars() : 문자열의 각 문자를 표현하는 unicode 값(정수)을 Stream으로 생성
+        // 2) .mapToObj(c -> (char) c) : 각 unicode 값을 char(이때도 Stream)로 변환
+        // 3) .toArray(Character[]::new) : 각 char을 Character[] 배열에 담음
         Character[] arr = s.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
         String answer = "";
-        String upper = "";
-
-        // <-> StringBuilder.reverse() : 단순히 역순으로 뒤집음
+        
         Arrays.sort(arr, Collections.reverseOrder());
         
         for (int i = 0; i < arr.length; i ++) {
-            if (!Character.isUpperCase(arr[i])) {
-                answer += arr[i];
-            }
-            else {
-                upper += arr[i];
-            }
+            answer += arr[i];
         }
         
-        return answer + upper;
+        return answer;
     }
 }
