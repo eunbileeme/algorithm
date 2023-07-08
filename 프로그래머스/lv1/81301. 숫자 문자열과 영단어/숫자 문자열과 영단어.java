74d8a89d61c8452a5b59e83e09aee9aecc8a3170
Eunbi Lee <pask2202@gmail.com>
2023-07-08 16:27:10 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.27 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
+        // HashMap에 초깃값 선언
         map.put("zero", 0);
         map.put("one", 1);
         map.put("two", 2);
@@ -15,15 +16,19 @@ class Solution {
         map.put("nine", 9);
         
         char[] word = s.toCharArray();
+        // "one" -> 1을 위한 변수
         String words = "";
+        // "1478" -> 1478을 위한 변수
         String answer = "";
         for (Character ch : word) {
+            // Character.isDigit() : 해당 문자가 숫자인지 판별
             if(Character.isDigit(ch)) {
                 answer += ch;
             } else {
                 words += ch;
                 if (map.containsKey(words)) {
                     answer += map.get(words);
+                    // 다음 변환을 위한 초기화 수행
                     words = "";
                 }
             }
