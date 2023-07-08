9ed7b757616caf23cdae53665ce9a635d8569d30
Eunbi Lee <pask2202@gmail.com>
2023-07-08 23:44:54 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.22 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -2,22 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String t, String p) {
-        int answer = 0;
+        int count = 0;
         List<String> list = new LinkedList<>();
         
         for (int i = 0; i <= t.length() - p.length(); i ++) {
             list.add(t.substring(i, i + p.length()));
-        }
-        
-        for (int i = 0; i < list.size(); i ++) {
-            // 문자열(t, p)의 길이가 10,000 -> int의 범위 초과
-            // int의 범위를 넘어가면 데이터 값이 변경됨
-            // 따라서 long으로 변환 수행
+            
             if (Long.parseLong(list.get(i)) <= Long.parseLong(p)) {
-                answer ++;
+                count ++;
             }
         }
         
-        return answer;
+        return count;
     }
 } 
