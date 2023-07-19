75ae715f6f6bc654b53a36c7424c6d59273efe28
Eunbi Lee <pask2202@gmail.com>
2023-07-19 21:01:53 +0900

    [unrated] Title: 추억 점수, Time: 1.86 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
+        for (int i = 0; i < name.length; i ++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for (int i = 0; i < photo.length; i ++) {
+            for (int j = 0; j < photo[i].length; j ++) {
+                if (map.containsKey(photo[i][j]))
+                    answer += map.get(photo[i][j]);
+            }
+            
+            list.add(answer);
+            answer = 0;
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
