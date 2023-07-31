9fc18c25f18e608745708925cad3e35ef6f78dae
Eunbi Lee <pask2202@gmail.com>
2023-07-31 23:36:58 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.27 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,33 @@
+// 1. score 배열만큼 반복문을 돌면서, 명예의 전당 점수를 저장할 리스트를 한 개 선언한다.
+// 2. 리스트 사이즈가 k보다 작다면, 리스트에 값을 넣고 그 중 최솟값을 answer에 넣는다.
+// 3. 리스트 사이즈가 k라면, 최솟값을 가져와서 score보다 크면 최솟값을 삭제하고 리스트에 점수를 넣는다.
+import java.util.*;
+
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> temp = new ArrayList<>();
+        
+        for (int i = 0; i < score.length; i ++) {
+            if (temp.size() < k) {
+                temp.add(score[i]);
+                Collections.sort(temp);
+                answer[i] = temp.get(0);
+                continue;
+            }
+            
+            if (temp.size() == k) {
+                int minNum = temp.get(0);
+                if (minNum < score[i]) {
+                    temp.remove(0);
+                    temp.add(score[i]);
+                    Collections.sort(temp);
+                }
+                answer[i] = temp.get(0);
+            }
+        }
+        
+        return answer;
+    }
+}
