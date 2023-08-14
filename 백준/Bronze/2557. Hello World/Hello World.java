f9209cdce514470b9703d5594689ce342036bb3c
Eunbi Lee <pask2202@gmail.com>
2023-08-14 21:11:57 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // ※ bw.write() : 버퍼에 출력문을 저장
+        bw.write("Hello World!");
+        // ※ bw.flush() : 버퍼에 담긴 모든 문자열을 출력
+        bw.flush();
+        bw.close();
+    }
+}
