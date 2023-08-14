fc0119a958221ba16a221dfc1ea514c74ddaa70c
Eunbi Lee <pask2202@gmail.com>
2023-08-14 17:24:57 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // ※ StringTokenizer를 사용할 경우, 문자열의 시작과 끝의 공백은 무시
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         // st.countTokens() : 토큰, 즉 공백을 기준으로 입력받은 문자열의 개수를 센다.
