63113d599fc56c70960d2146946b316a76c7d395
Eunbi Lee <pask2202@gmail.com>
2023-08-14 17:01:21 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+
+        System.out.println(a / b);
+        br.close();
+    }
+}
