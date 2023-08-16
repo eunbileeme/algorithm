85d7f75b76216dce0996dc20a5d36c1e3779a1b2
Eunbi Lee <pask2202@gmail.com>
2023-08-16 21:17:58 +0900

    [Silver V] Title: 거스름돈, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int cnt = 0;
 
@@ -21,14 +20,11 @@ public class Main {
         }
 
         if (n < 0) {
-            bw.write(-1 + "");
-            bw.flush();
+            System.out.println(-1);
         } else {
-            bw.write(cnt + "");
-            bw.flush();
+            System.out.println(cnt);
         }
 
-        bw.close();
         br.close();
     }
 }
