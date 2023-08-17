a8bb2137a92e99211a032ced96e7a456615edc5c
Eunbi Lee <pask2202@gmail.com>
2023-08-17 17:24:36 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+        bw.flush();
+        br.close();
+    }
+}
