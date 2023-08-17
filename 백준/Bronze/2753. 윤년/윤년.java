00a73a539b3c8056418443297d99bab92f801edf
Eunbi Lee <pask2202@gmail.com>
2023-08-17 16:25:35 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+
+        br.close();
+    }
+}
