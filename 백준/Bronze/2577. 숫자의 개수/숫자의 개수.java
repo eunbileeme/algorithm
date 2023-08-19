49275d13d5ba5a0adcfc999bb5b99eafb57c0f92
Eunbi Lee <pask2202@gmail.com>
2023-08-19 17:43:11 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[3];
+        int sum = 1;
+
+        for (int i = 0; i < arr.length; i ++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum *= arr[i];
+        }
+
+        String stringSum = String.valueOf(sum);
+        int[] cnt = new int[10];
+
+        for (int i = 0; i < stringSum.length(); i ++) {
+            if ('0' == stringSum.charAt(i))
+                cnt[0] ++;
+            else if ('1' == stringSum.charAt(i))
+                cnt[1] ++;
+            else if ('2' == stringSum.charAt(i))
+                cnt[2] ++;
+            else if ('3' == stringSum.charAt(i))
+                cnt[3] ++;
+            else if ('4' == stringSum.charAt(i))
+                cnt[4] ++;
+            else if ('5' == stringSum.charAt(i))
+                cnt[5] ++;
+            else if ('6' == stringSum.charAt(i))
+                cnt[6] ++;
+            else if ('7' == stringSum.charAt(i))
+                cnt[7] ++;
+            else if ('8' == stringSum.charAt(i))
+                cnt[8] ++;
+            else
+                cnt[9] ++;
+        }
+
+        for (int i = 0; i < cnt.length; i ++) {
+            System.out.println(cnt[i]);
+        }
+
+        br.close();
+    }
+}
