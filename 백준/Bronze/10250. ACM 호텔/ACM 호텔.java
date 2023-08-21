b8b2852b2be74e5269d2ef7fad6fe0a7ba13ec31
Eunbi Lee <pask2202@gmail.com>
2023-08-21 15:06:06 +0900

    [Bronze III] Title: ACM 호텔, Time: 136 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -13,7 +13,12 @@ public class Main {
             int w = Integer.parseInt(st.nextToken());
             int person = Integer.parseInt(st.nextToken());
 
+            // ※ -1을 하는 이유 : 0-based 인덱싱
+            // 즉, 프로그래밍에서 첫 번째는 0에 해당하기 때문에 -1을 수행
+            
+            // ※ 몫 연산 : 그 층에서 몇 번째 방에 위치해야 하는지
             int width = (person - 1) / h + 1;
+            // ※ 나머지 연산 : 손님이 어떤 층에 위치해야 하는지
             int height = (person - 1) % h + 1;
 
             System.out.println(height * 100 + width);
