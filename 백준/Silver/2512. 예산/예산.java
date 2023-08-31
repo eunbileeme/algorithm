49c0a8bbdf8822be1cc9a8b18e61e4b9dcdd7116
Eunbi Lee <pask2202@gmail.com>
2023-08-31 21:49:29 +0900

    Update 예산.java

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -7,19 +7,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[n];
-        
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         int m = Integer.parseInt(br.readLine());
-
+        
         Arrays.sort(arr);
         
         System.out.println(getValue(m, 0, arr[n - 1], arr));
-
         br.close();
     }
 
@@ -29,6 +28,7 @@ public class Main {
         while (l <= r) {
             int m = l + (r - l) / 2; // m : 현재 검사하는 상한액
             int sum = 0;
+            
             for (int i : arr) {
                 sum += (i < m) ? i : m;
             }
