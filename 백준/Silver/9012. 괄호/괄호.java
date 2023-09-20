57fd2d6ac42ce893ff9d592457a49230c3c30ecd
Eunbi Lee <pask2202@gmail.com>
2023-09-21 00:41:18 +0900

    Update 괄호.java

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,23 +7,22 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String[] str = new String[n];
 
-        for (int i = 0; i < n; i ++) {
+        for (int i = 0; i < n; i++) {
             str[i] = br.readLine();
             int cnt = 0;
-            for (int j = 0; j < str[i].length(); j ++) {
-                if (cnt < 0)
-                    break;
+            
+            for (int j = 0; j < str[i].length(); j++) {
+                if (cnt < 0) break;
+                
                 if (str[i].charAt(j) == '(') {
-                    cnt ++;
-                } else if (str[i].charAt(j) == ')')
-                    cnt --;
+                    cnt++;
+                } else if (str[i].charAt(j) == ')') cnt--;
             }
-            if (cnt == 0)
-                System.out.println("YES");
-            else if ((cnt > 0) || (cnt < 0))
-                System.out.println("NO");
+            
+            if (cnt == 0) System.out.println("YES");
+            else if ((cnt > 0) || (cnt < 0)) System.out.println("NO");
         }
-        
+
         br.close();
     }
 }
