173be4652a6f864338b2c25624223141dffef1a4
Eunbi Lee <pask2202@gmail.com>
2023-11-10 00:33:13 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,26 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     private static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     private static boolean[] visited;
-    private static int sum;
+    private static int sum = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // n : 컴퓨터의 수
-        int k = Integer.parseInt(br.readLine()); // k : 네트워크 상에서 직접 연결되어 있는 컴퓨터의 수
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        // 1. 그래프(객체) 초기화
+        // 1. 그래프 초기화
         for (int i = 0; i <= n; i ++) {
             graph.add(new ArrayList<>());
         }
 
-        // 2. 그래프(객체)에 값 추가
-        for (int i = 0; i < k; i ++) {
+        // 2. 간선 정보 추가
+        for (int i = 0; i < m; i ++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
             int node1 = Integer.parseInt(st.nextToken());
@@ -30,26 +28,22 @@ public class Main {
             graph.get(node2).add(node1);
         }
 
-        /*
-        * [[], [2, 5], [1, 3, 5], [2], [7], [1, 2, 6], [5], [4]]
-        */
-
-        // 3. 이웃 정점(노드)들을 오름차순으로 정렬
+        // 3. 정렬
         for (int i = 1; i <= n; i ++) {
             Collections.sort(graph.get(i));
         }
 
-        // 4. 방문 유무 초기화
+        // 4. 초기화
         visited = new boolean[n + 1];
 
-        // 5. dfs 수행
+        // 5. dfs
         dfs(1);
 
+        // 6. 출력
         System.out.println(sum);
-        br.close();
     }
 
-    private static void dfs(int node) {
+    private static void dfs (int node) {
         visited[node] = true;
 
         for (int adjNode : graph.get(node)) {
