0c8372ff74ed829984e321e6fc91739c84b05d7b
Eunbi Lee <pask2202@gmail.com>
2023-11-12 00:15:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18548 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,57 +1,61 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int n;
-    private static int k;
-    public static int[] visited;
+    private static int n = 0;
+    private static int k = 0;
+    private static int[] visited;
+    private static int time = 0;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
+        // 1. 초기화
         visited = new int[100001];
 
-        if (n == k) {
-            System.out.println(0);
-        } else {
-            bfs(n);
-        }
+        // 2. 탐색
+        time = bfs(n);
 
+        // 3. 출력
+        System.out.println(time);
         br.close();
     }
 
-    private static void bfs(int n) {
+    private static int bfs(int n) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(n);
+        q.offer(n);
         visited[n] = 1;
 
         while (!q.isEmpty()) {
-            int temp = q.poll();
+            int location = q.poll();
 
-            for (int i = 0; i < 3; i++) {
-                int next = 0;
+            if (location == k) {
+                return visited[location] - 1;
+            }
 
-                if (i == 0) {
-                    next = temp + 1;
-                } else if (i == 1) {
-                    next = temp - 1;
-                } else {
-                    next = temp * 2;
-                }
+            if (location - 1 >= 0 && visited[location - 1] == 0) {
+                visited[location - 1] = visited[location] + 1; // 시간을 구해야하므로 + 1
+                q.offer(location - 1);
+            }
 
-                if (next == k) {
-                    System.out.println(visited[temp]);
-                    return;
-                }
+            if (location + 1 <= 100000 && visited[location + 1] == 0) {
+                visited[location + 1] = visited[location] + 1;
+                q.offer(location + 1);
+            }
 
-                if (next >= 0 && next < visited.length && visited[next] == 0) {
-                    q.add(next);
-                    visited[next] = visited[temp] + 1;
-                }
+            if (location * 2 <= 100000 && visited[location * 2] == 0) {
+                visited[location * 2] = visited[location] + 1;
+                q.offer(location * 2);
             }
         }
+
+        return -1;
     }
 }
