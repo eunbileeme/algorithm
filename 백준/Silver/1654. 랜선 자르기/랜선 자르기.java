2e24d7aff13b2315063026645cf6f7d2c2ea114f
Eunbi Lee <pask2202@gmail.com>
2023-11-27 02:19:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 196 ms, Memory: 17232 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,44 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(st.nextToken());
+        // 1. 초기화
         int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] cables = new int[k];
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i ++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        // 2. 입력
+        for (int i = 0; i < cables.length; i++) {
+            cables[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        // 3. 정렬
+        Arrays.sort(cables);
 
-        System.out.println(getValue(arr, k));
-
+        // 4. 출력
+        System.out.println(getMaxCableLength(cables, n));
         br.close();
     }
 
-    private static long getValue(int[] arr, int targetCnt) {
-        long l = 1; // ※ 랜선 길이가 0이 되지 않도록 1로 초기화
-        long r = arr[arr.length - 1];
+    /*
+    * 만들 수 있는 랜선의 최대 길이 구하기
+    * lower bound(= 하한 값) : 찾고자 하는 값 이상의 값이 처음으로 나타나는 위치
+    * = lower bound로 찾고자 하는 값을 기준으로 점진적으로 증가시켜나가면서 최댓값 찾기
+    * m (구하고자 하는 값) : 랜선 길이(값) <-> 인덱스
+     */
+
+    private static long getMaxCableLength(int[] cables, int target) {
+        long l = 1;
+        long r = cables[cables.length - 1];
         long maxLength = 0;
 
         while (l <= r) {
             long m = l + (r - l) / 2;
-            int cnt = 0;
+            int cableCnt = 0;
 
-            for (int i = 0; i < arr.length; i ++) {
-                cnt += (arr[i] / m);
+            for (int i = 0; i < cables.length; i++) {
+                cableCnt += (cables[i] / m);
             }
-
-            if (cnt < targetCnt) {
-                r = m - 1;
-            } else {
+            
+            if (target <= cableCnt) {
+                // 목표 개수가 자른 케이블의 개수보다 작다면
+                // 랜선을 작게 작게 잘랐다는 것 = 자를 랜선의 길이를 늘려야 함
                 maxLength = m;
                 l = m + 1;
+            } else {
+                r = m - 1; // target > cableCnt
             }
         }
 
