9ff1694208975c7e903372d777ade7b172dcebc6
Eunbi Lee <pask2202@gmail.com>
2024-04-05 10:10:11 +0900

    [Silver V] Title: 폴리오미노, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,48 +1,72 @@
-// 그리디 : 가장 큰 블록인 "AAAA"로 대체한 후 나머지가 있다면 "BB"로 대체
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 1. 입력
         String str = br.readLine();
-        String[] strArr = str.split("\\.");
+
+        // 2. 출력용 변수
         StringBuilder sb = new StringBuilder();
 
-        // XXXX..XX.XX
-        // ["XXXX", "", "XX", "XX"]
-        for (int i = 0; i < strArr.length; i ++) {
-            if (strArr[i].length() % 2 != 0) {
-                // 무조건 중간에 2 또는 4로 나눌 수 없다면, 새로운 StringBuilder에 "-1"을 붙인 후 반환
-                sb = new StringBuilder("-1");
-                break;
-            } else if (strArr[i].length() % 4 == 0) {
-                // ※ 'A'씩 붙이는게 아니라 "AAAA"로 붙일 수 있음
-                sb.append("AAAA".repeat(strArr[i].length() / 4));
+        // 3. X 카운트용 변수
+        int cntX = 0;
 
+        for (int i = 0; i < str.length(); i++) {
+            // 4. 문자열의 문자가 'X'일 경우
+            if (str.charAt(i) == 'X') {
+                // 5. X의 개수를 증가
+                cntX++;
             } else {
-                // ※ 몫만큼 "AAAA"를 붙여줌으로써 "XX"일 경우는 "BB"만 추가
-                sb.append("AAAA".repeat(strArr[i].length() / 4));
-                sb.append("BB");
-            }
-            // 알파벳 사이의 "."를 붙여주기 위한 조건문
-            // ※ 연속된 n개의 "."가 있다면, n - 1개의 ""로 구성됨
-
-            // XXXX..XX.XX -> ["XXXX", "", "XX", "XX"]
-            // i = 0일 때, "AAAA" -> if -> "AAAA."
-            // i = 1일 때, if -> "AAAA.."
-            // i = 2일 때, "AAAA..BB" -> if -> "AAAA..BB."
-            if (i != strArr.length - 1)
+                // 6. X가 아니라면(= '.'이라면), 현재까지의 X를 변환
+                // TODO StringBuilder에 문자열 저장은 .append()
+                sb.append(replace(cntX));
+                // 14. X의 개수를 초기화
+                cntX = 0;
+                // 15. 현재 마주한 '.' 추가
                 sb.append(".");
+            }
         }
 
-        String answer = sb.toString();
-        if (!answer.equals("-1")) {
-            // 알파벳의 맨 끝에 "."가 있었다면, 다시 붙여주기 위한 조건문
-            sb.append(".".repeat(str.length() - answer.length()));
+        // TODO 16. '.'가 없는 문자열에서, X를 처리
+        // 이 과정이 없을 경우, 아래의 경우는 공백이 출력
+        // ex. "XXXXXX" -> for문의 if만 수행하여, cntX만 증가했기 때문에 sb에는 어떠한 값도 담기지 않았기 때문
+        sb.append(replace(cntX));
+
+        // 17. 'X'가 있다면 -1 반환
+        if (sb.toString().contains("X")) {
+            System.out.println(-1);
+            return;
         }
 
-        System.out.println(sb.toString());
-
+        // 18. 출력
+        System.out.println(sb);
         br.close();
+    }
+
+    private static String replace(int cntX) {
+        StringBuilder tmp = new StringBuilder();
+
+        // 7. X가 4 이상이라면
+        while (cntX >= 4) {
+            // 8. A 4개 추가
+            tmp.append("AAAA");
+            // 9. cntX에서 -4
+            cntX -= 4;
+        }
+
+        // 10. X가 2라면
+        if (cntX == 2) {
+            // 11. B 2개 추가
+            tmp.append("BB");
+            // 12. 남은 X가 있다면
+        } else if (cntX != 0) {
+            // 13. 그대로 X
+            tmp.append("X");
+        }
+
+        return tmp.toString();
     }
 }
