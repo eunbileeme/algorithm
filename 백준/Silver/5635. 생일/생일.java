282fa372c561b19627476d00e8e7f11608e492d7
Eunbi Lee <pask2202@gmail.com>
2024-04-18 09:29:15 +0900

    [Silver V] Title: 생일, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 0.
+        Map<String, ArrayList<Integer>> map = new HashMap<>();
+
+        // 1.
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            // ex. Mickey 1 10 1991
+            // 2. Map<String, ArrayList>> : 이름 - 년도, 월, 일 순으로 저장
+            String name = st.nextToken();
+            int day = Integer.parseInt(st.nextToken());
+            int month = Integer.parseInt(st.nextToken());
+            int year = Integer.parseInt(st.nextToken());
+
+            // 2-1. 주어진 순서대로 일, 월, 년도 순으로 저장
+            ArrayList<Integer> birthday = new ArrayList<>(Arrays.asList(year, month, day));
+            map.put(name, birthday);
+
+            // 2-2. Collections.reverseOrder()
+        }
+
+        // 3. 년도 -> 일 -> 월 순으로 비교
+        Comparator<Map.Entry<String, ArrayList<Integer>>> comparator = (a, b) -> {
+            ArrayList<Integer> date1 = a.getValue();
+            ArrayList<Integer> date2 = b.getValue();
+
+            if (!date1.get(0).equals(date2.get(0))) {
+                return date1.get(0) - date2.get(0);
+            } else if (!date1.get(1).equals(date2.get(1))) {
+                return date1.get(1) - date2.get(1);
+            } else {
+                return date1.get(2) - date2.get(2);
+            }
+        };
+
+        // 4. value 기준, 가장 나이가 적은 사람 / 가장 나이가 많은 사람
+        // key 기준, 이름을 가져와서 출력
+        String old = Collections.max(map.entrySet(), comparator).getKey();
+        String young = Collections.min(map.entrySet(), comparator).getKey();
+
+        System.out.println(old);
+        System.out.println(young);
+
+        br.close();
+    }
+}
