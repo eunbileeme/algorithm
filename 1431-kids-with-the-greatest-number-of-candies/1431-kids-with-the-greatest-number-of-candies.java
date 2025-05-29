b5d6d0d7bbcd64e537f84d4aaf33238cfada1670
Eunbi Lee <pask2202@gmail.com>
2025-05-29 22:11:51 +0900

    Time: 2 ms (16.66%), Space: 42.8 MB (14.36%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,24 +1,27 @@
-// 여러 아이가 가장 많은 사탕을 가질 수 있다는 점에 유의하세요.
-// 각 어린이에 대해 캔디[i] + 추가 캔디가 캔디[i]의 최대값을 초과하는지 확인
+import java.util.*;
+
+// 1. n번째 아이는 one-base (즉, 1부터 카운트)
+// 2. n번째 아이가 가진 n번째 캔디 + 엑스트라 캔디
+// 3. 2의 값이 현재 n번째 아이들 중 가장 많이 가지고 있다면 true, 아니라면 false
 
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        // 1. boolean 결과용 배열
-        List<Boolean> ans = new ArrayList<>();
+        int size = candies.length;
+        List<Boolean> result = new ArrayList<>();
+        List<Integer> candyCounts = new ArrayList<>();
+        int maxCandy = Arrays.stream(candies).max().getAsInt();
 
-        // 2. if (candies[i] + extraCandies >= Maximum in Candies[i])
-        int maxCandy = candies[0];
-        for (int i = 0; i < candies.length; i ++) {
-            maxCandy = Math.max(maxCandy, candies[i]);
-            candies[i] += extraCandies;
+        for (int i = 0; i < size; i ++) {
+            int currentCandyCount = candies[i] + extraCandies;
+            candyCounts.add(currentCandyCount);
+
+            if (currentCandyCount >= maxCandy) {
+                result.add(true);
+                continue;
+            }
+            result.add(false);
         }
 
-        // 3.
-        for (int i = 0; i < candies.length; i ++) {
-            if (candies[i] >= maxCandy) ans.add(true);
-            else ans.add(false);
-        }
-
-        return ans;
+        return result;
     }
 }
